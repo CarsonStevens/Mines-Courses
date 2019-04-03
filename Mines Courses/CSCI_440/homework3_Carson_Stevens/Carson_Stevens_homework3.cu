f19d561cff45a1374f25ee81cc23561d0cc7da9f
CarsonStevens <carsonstevens@mines.edu>
2019-04-02 19:40:28 -0600

    Fixed random number data

--- Mines Courses/CSCI_440/homework3_Carson_Stevens/Carson_Stevens_homework3.cu
+++ Mines Courses/CSCI_440/homework3_Carson_Stevens/Carson_Stevens_homework3.cu
@@ -72,7 +72,7 @@ int main(int argc, char* argv[]){
     }
     //Initialize the multiply vector with data
     for(int i = 0; i < num_cols; i++){
-        mult_data[i] = float(random.random())/1111111111;
+        mult_data[i] = rand() % 1000000000 / 1111111111;
     }
 
     //Markers for keeping track of data
