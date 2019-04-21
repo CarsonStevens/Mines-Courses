3ed1e5e48584e938885f40ebe467c7cad3fd50e3
CarsonStevens <carsonstevens@mines.edu>
2019-04-20 19:16:25 -0600

    Trying old method

--- Mines Courses/CSCI_440/homework4_Carson_Stevens/homework4_Carson_Stevens.cu
+++ Mines Courses/CSCI_440/homework4_Carson_Stevens/homework4_Carson_Stevens.cu
@@ -9,73 +9,73 @@
 
 using namespace std;
 
-__global__ void scan_with_addition(int *g_idata, int *g_odata, int n)
-{
-    extern __shared__ int temp[]; // allocated on invocation
-    int thid = threadIdx.x;
-    int pout = 0, pin = 1;
-    // load input into shared memory.
-    // This is exclusive scan, so shift right by one and set first elt to 0
-    temp[pout*n + thid] = (thid > 0) ? g_idata[thid-1] : 0;
-    __syncthreads();
-    for (int offset = 1; offset < n; offset *= 2)
-    {
-        pout = 1 - pout; // swap double buffer indices
-        pin = 1 - pout;
-        if (thid >= offset) {
-            temp[pout * n + thid] += temp[pin * n + thid - offset];
-        }
-        else {
-            temp[pout * n + thid] = temp[pin * n + thid];
-        }
-        __syncthreads();
-    }
-    g_odata[thid] = temp[pout*n+thid]; // write output
-}
-//
-//__global__ void scan_with_addition(int *sum_array, int *A_gpu, int n){
-//
-//    extern __shared__ int temp[];
-//    int thIdx = threadIdx.x;
-//    int offset = 1;
-//
-//    //load input into shared memory
-//    temp[2*thIdx] = sum_array[2*thIdx];
-//    temp[2*thIdx+1] = sum_array[2*thIdx+1];
-//
-//    //build sum inplace up the tree
-//    for(int d = n>>1; d > 0; d = 1){
-//        __syncthreads();
-//
-//        if(thIdx < d){
-//            int ai = offset * (2*thIdx+1)-1;
-//            int bi = offset * (2*thIdx+2)-1;
-//
-//            temp[bi] += temp[ai];
-//        }
-//        offset *= 2;
-//    }
-//    // clear the last element
-//    if(thIdx == 0){ temp[n-1] = 0; }
-//    for(int d = 1; d < n; d *= 2){
-//        offset >>= 1;
-//        __syncthreads();
-//        if(thIdx < d){
-//            int ai = offset *(2*thIdx+1)-1;
-//            int bi = offset *(2*thIdx+2)-1;
-//
-//            int t = temp[ai];
-//            temp[ai] = temp[bi];
-//            temp[bi] = t;
-//        }
-//    }
+//__global__ void scan_with_addition(int *g_idata, int *g_odata, int n)
+//{
+//    extern __shared__ int temp[]; // allocated on invocation
+//    int thid = threadIdx.x;
+//    int pout = 0, pin = 1;
+//    // load input into shared memory.
+//    // This is exclusive scan, so shift right by one and set first elt to 0
+//    temp[pout*n + thid] = (thid > 0) ? g_idata[thid-1] : 0;
 //    __syncthreads();
-//
-//    A_gpu[2*thIdx] = temp[2*thIdx];
-//    A_gpu[2*thIdx+1] = temp[2*thIdx+1];
+//    for (int offset = 1; offset < n; offset *= 2)
+//    {
+//        pout = 1 - pout; // swap double buffer indices
+//        pin = 1 - pout;
+//        if (thid >= offset) {
+//            temp[pout * n + thid] += temp[pin * n + thid - offset];
+//        }
+//        else {
+//            temp[pout * n + thid] = temp[pin * n + thid];
+//        }
+//        __syncthreads();
+//    }
+//    g_odata[thid] = temp[pout*n+thid]; // write output
 //}
-//
-//
+
+__global__ void scan_with_addition(int *sum_array, int *A_gpu, int n){
+
+    extern __shared__ int temp[];
+    int thIdx = threadIdx.x;
+    int offset = 1;
+
+    //load input into shared memory
+    temp[2*thIdx] = sum_array[2*thIdx];
+    temp[2*thIdx+1] = sum_array[2*thIdx+1];
+
+    //build sum inplace up the tree
+    for(int d = n>>1; d > 0; d = 1){
+        __syncthreads();
+
+        if(thIdx < d){
+            int ai = offset * (2*thIdx+1)-1;
+            int bi = offset * (2*thIdx+2)-1;
+
+            temp[bi] += temp[ai];
+        }
+        offset *= 2;
+    }
+    // clear the last element
+    if(thIdx == 0){ temp[n-1] = 0; }
+    for(int d = 1; d < n; d *= 2){
+        offset >>= 1;
+        __syncthreads();
+        if(thIdx < d){
+            int ai = offset *(2*thIdx+1)-1;
+            int bi = offset *(2*thIdx+2)-1;
+
+            int t = temp[ai];
+            temp[ai] = temp[bi];
+            temp[bi] = t;
+        }
+    }
+    __syncthreads();
+
+    A_gpu[2*thIdx] = temp[2*thIdx];
+    A_gpu[2*thIdx+1] = temp[2*thIdx+1];
+}
+
+
 
 int main(int argc, char* argv[]) {
     ///////////////////////////////////////////
