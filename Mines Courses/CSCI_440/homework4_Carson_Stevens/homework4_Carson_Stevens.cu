6e63cd3617d8a9d3e497d409f37fd0e27e6b32b9
CarsonStevens <carsonstevens@mines.edu>
2019-04-20 19:11:49 -0600

    Debugging

--- Mines Courses/CSCI_440/homework4_Carson_Stevens/homework4_Carson_Stevens.cu
+++ Mines Courses/CSCI_440/homework4_Carson_Stevens/homework4_Carson_Stevens.cu
@@ -30,7 +30,7 @@ __global__ void scan_with_addition(int *g_idata, int *g_odata, int n)
         }
         __syncthreads();
     }
-    g_odata[thid] = temp[pout*n+thid]; // write output
+    g_odata[thid] = temp[pout*n+thid+1]; // write output
 }
 //
 //__global__ void scan_with_addition(int *sum_array, int *A_gpu, int n){
