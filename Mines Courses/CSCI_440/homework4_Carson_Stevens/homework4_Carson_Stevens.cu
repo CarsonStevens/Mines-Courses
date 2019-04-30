8ddd362edc1232d0ba6216bf79119a8a7341c21f
CarsonStevens <carsonstevens@mines.edu>
2019-04-30 17:36:35 -0600

    New implementation HW4

--- Mines Courses/CSCI_440/homework4_Carson_Stevens/homework4_Carson_Stevens.cu
+++ Mines Courses/CSCI_440/homework4_Carson_Stevens/homework4_Carson_Stevens.cu
@@ -9,56 +9,32 @@
 
 using namespace std;
 
-__global__ void scan_with_addition(unsigned int* g_idata, unsigned int* g_odata, int n){
-    extern __shared__ unsigned int smem[];
+__global__ void scan_with_addition(unsigned int* sum_array, unsigned int* A_gpu, int N) {
+    int tid = threadIdx.x;
+    extern __shared__ unsigned int temp[];
 
-    // load shared mem
-    unsigned int tid = threadIdx.x;
-    unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
+    int out = 0;
+    int in = 1;
 
-    smem[tid] = (i < n) ? g_idata[i] : 0;
-
+    temp[tid] = (tid > 0) ? sum_array[tid-1] : 0;
     __syncthreads();
 
-    // do reduction in shared mem
-    for(unsigned int s=1; s < blockDim.x; s *= 2) {
-        // modulo arithmetic is slow!
-        if ((tid % (2*s)) == 0) {
-            smem[tid] += smem[tid + s];
+    for(int offset=1; offset < N; offset *= 2){
+        out = 1 - out;
+        in = 1 - out;
+
+        if(tid >= offset){
+            temp[out*N+tid] = temp[in*N+tid-offset] + temp[in*N+tid];
         }
+        else{
+            temp[out*N+tid] = temp[in*N+tid];
+        }
+
         __syncthreads();
     }
 
-    // write result for this block to global mem
-    if (tid == 0) g_odata[blockIdx.x] = smem[0];
-
-//    // perform first level of reduction,
-//    // reading from global memory, writing to shared memory
-//    unsigned int tid = threadIdx.x;
-//    unsigned int i = blockIdx.x*(blockDim.x*2) + threadIdx.x;
-//
-//    smem[tid] = (i < n) ? g_idata[i] : 0;
-//    if (i + blockDim.x < n)
-//        smem[tid] += g_idata[i+blockDim.x];
-//
-//    __syncthreads();
-//
-//    // do reduction in shared mem
-//    for(unsigned int s=blockDim.x/2; s>0; s>>=1)
-//    {
-//        if (tid < s)
-//        {
-//            smem[tid] += smem[tid + s];
-//        }
-//        __syncthreads();
-//    }
-//
-//    // write result for this block to global mem
-//    if (tid == 0) g_odata[blockIdx.x] = smem[0];
-
+    A_gpu[tid] = temp[out*N+tid];
 }
-
-
 
 
 int main(int argc, char* argv[]) {
@@ -99,20 +75,9 @@ int main(int argc, char* argv[]) {
     cudaMalloc((void **)&dev_sum_array, sizeof(unsigned int)*N);
     cudaMalloc((void **)&dev_A_gpu, sizeof(unsigned int)*N);
     cudaMemcpy(dev_sum_array, sum_array, sizeof(unsigned int)*N, cudaMemcpyHostToDevice);
-    cudaMemcpy(dev_A_gpu, A_gpu, sizeof(unsigned int)*N, cudaMemcpyHostToDevice);
-
-
-    // Round up according to array size
-    //gridSize = (N + blockSize - 1) / blockSize;
-    int threads = 16;
-    int blocks = 2;
-    dim3 dimBlock(threads, 1, 1);
-    dim3 dimGrid(blocks, 1, 1);
-    int smemSize = threads * sizeof(unsigned int);
 
     // Call function
-    // second blockSize for shared memory
-    scan_with_addition<<< dimGrid, dimBlock, smemSize >>>(dev_sum_array, dev_A_gpu, N);
+    scan_with_addition<<< 1, N, 2*N*sizeof(unsigned int) >>>(dev_sum_array, dev_A_gpu, N);
     cudaDeviceSynchronize();
 
     // copy result back
@@ -120,7 +85,6 @@ int main(int argc, char* argv[]) {
 
     // free memory
     cudaFree(dev_sum_array);
-    cudaFree(dev_A_gpu);
 
 
     /////////////////////////////////////////////////
