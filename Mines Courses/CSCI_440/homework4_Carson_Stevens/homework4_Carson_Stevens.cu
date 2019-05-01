d16ff54fe022c6a05a7463e2f2f68fa9d80a821d
CarsonStevens <carsonstevens@mines.edu>
2019-04-30 19:43:55 -0600

    Trying new version

--- Mines Courses/CSCI_440/homework4_Carson_Stevens/homework4_Carson_Stevens.cu
+++ Mines Courses/CSCI_440/homework4_Carson_Stevens/homework4_Carson_Stevens.cu
@@ -37,47 +37,51 @@ __global__ void scan_with_addition(int* sum_array, int* A_gpu, const int N) {
     A_gpu[tid] = temp[out*N+tid];
 }
 
-__global__ void prescan(int *g_odata, int *g_idata, int n){
-    extern __shared__ int temp[];// allocated on invocation
-
-
+__global__ void prescan(float *g_odata, float *g_idata, int n){
+    extern  __shared__  int temp[];
+    // allocated on invocation
     int thid = threadIdx.x;
+    int bid = blockIdx.x;
+
+
     int offset = 1;
+    if((bid * thread_num + thid)<n){ temp[thid]  = g_idata[bid * thread_num + thid];
+    }else{ temp[thid]  = 0;
+    } // Make the "empty" spots zeros, so it won't affect the final result.
 
-    temp[2*thid] = g_idata[2*thid]; // load input into shared memory
-    temp[2*thid+1] = g_idata[2*thid+1];
+    for (int d = thread_num>>1; d > 0; d >>= 1){
     // build sum in place up the tree
-    for (int d = n>>1; d > 0; d >>= 1){
-
 
         __syncthreads();
-
         if (thid < d){
             int ai = offset*(2*thid+1)-1;
             int bi = offset*(2*thid+2)-1;
-
             temp[bi] += temp[ai];
         }
         offset *= 2;
     }
 
-    if (thid == 0) { temp[n - 1] = 0; } // clear the last element
+    if (thid == 0){
+        temp[thread_num - 1] = 0;
+    }
 
+    // clear the last element
+    for (int d = 1; d < thread_num; d *= 2){
     // traverse down tree & build scan
-    for (int d = 1; d < n; d *= 2){
+
         offset >>= 1;
         __syncthreads();
         if (thid < d){
             int ai = offset*(2*thid+1)-1;
             int bi = offset*(2*thid+2)-1;
             float t = temp[ai];
-            temp[ai] = temp[bi];
+            temp[ai]  = temp[ bi];
             temp[bi] += t;
         }
     }
     __syncthreads();
-    g_odata[2*thid] = temp[2*thid]; // write results to device memory
-    g_odata[2*thid+1] = temp[2*thid+1];
+
+    g_odata[bid * thread_num + thid] = temp[thid];
 }
 
 
@@ -129,12 +133,14 @@ int main(int argc, char *argv[]) {
     cudaMemcpy(dev_sum_array, sum_array, sizeof(int)*N, cudaMemcpyHostToDevice);
     cudaMemcpy(dev_A_gpu, A_gpu, sizeof(int)*N, cudaMemcpyHostToDevice);
 
-    dim3  blocksize(N);
-    dim3 gridsize(1);
+//    dim3  blocksize(N);
+//    dim3 gridsize(1);
+    int thread_num = 4;
+    int block_num = 2;
     // Call function
     start = chrono::high_resolution_clock::now();
     //reduce<<< gridsize, blocksize >>>(dev_sum_array,dev_A_gpu);
-    prescan<<< gridsize, blocksize, 2*N*sizeof(int) >>>(dev_sum_array, dev_A_gpu, N);
+    prescan<<< block_num, thread_num, 2*thread_num*sizeof(int) >>>(dev_sum_array, dev_A_gpu, N);
     cudaDeviceSynchronize();
     stop = chrono::high_resolution_clock::now();
     auto real = stop - start;
